9ef516a3f6866d39b0e689caa3042f3be027755a
DoomedPlayer <ni.beeger@duocuc.cl>
2026-05-26 17:23:23 -0400

    Ingreso de datos por medio de data.sql

---
+++ Springboot-API-REST-VENTA/target/classes/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO venta (direccion_compra, valor_compra, fecha_compra, despacho_generado) 
+VALUES ('Av. Los Leones 1234, Ñuñoa', 15000, '2026-05-20', true);
+
+INSERT INTO venta (direccion_compra, valor_compra, fecha_compra, despacho_generado) 
+VALUES ('Gran Avenida 7890, El Bosque', 25000, '2026-05-21', true);
+
+INSERT INTO venta (direccion_compra, valor_compra, fecha_compra, despacho_generado) 
+VALUES ('Av. Irarrazaval 3000, Ñuñoa', 12500, '2026-05-23', true);
+
+INSERT INTO venta (direccion_compra, valor_compra, fecha_compra, despacho_generado) 
+VALUES ('Los Morros 10200, El Bosque', 8990, '2026-05-25', false);
+
+INSERT INTO venta (direccion_compra, valor_compra, fecha_compra, despacho_generado) 
+VALUES ('Pedro de Valdivia 333, Ñuñoa', 45000, '2026-05-26', false);
